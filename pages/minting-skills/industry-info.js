07d85d56e34a089512254523e206ca53cbc0c43a
AlejandroCRios <al.cardonarios@gmail.com>
2022-06-08 16:18:19 -0500

    feat: :fire: clean code v2

--- pages/minting-skills/industry-info.js
+++ pages/minting-skills/industry-info.js
@@ -29,7 +29,6 @@ import { industries, roles } from "../../components/options-data/options";
 import { BsGithub, BsLinkedin, BsPerson, BsTwitter } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 import { FaIndustry, FaDumbbell } from "react-icons/fa";
-import MainBox from "../../components/layout/app-box";
 
 import { chakraStyles } from "../../components/styles/react-select";
 import { useUserInfo } from "../../context/user-context";
@@ -40,75 +39,73 @@ export default function IndustryInfo() {
   const { userInfo } = useUserInfo();
 
   return (
-    <MainBox>
-      <Box>
-        <VStack spacing={{ base: 4, md: 8, lg: 20 }}>
-          <Box display={"table"} justifyContent={"center"} align={"center"}>
-            {" "}
-            <Heading
-              display={"table-cell"}
-              verticalAlign="middle"
-              fontSize={{
-                base: "xl",
-                md: "2xl",
-              }}
-            >
-              Tell us what is your industry and your role in it.
-            </Heading>
-          </Box>
-          <Stack
-            spacing={{ base: 4, md: 8, lg: 20 }}
-            direction={{ base: "column", md: "row" }}
+    <Box>
+      <VStack spacing={{ base: 4, md: 8, lg: 20 }}>
+        <Box display={"table"} justifyContent={"center"} align={"center"}>
+          {" "}
+          <Heading
+            display={"table-cell"}
+            verticalAlign="middle"
+            fontSize={{
+              base: "xl",
+              md: "2xl",
+            }}
           >
-            <Box>
-              <VStack spacing={5}>
-                <FormControl isRequired>
-                  <FormLabel fontSize={"sm"}>
-                    What industry do you have experience in?
-                  </FormLabel>
+            Tell us what is your industry and your role in it.
+          </Heading>
+        </Box>
+        <Stack
+          spacing={{ base: 4, md: 8, lg: 20 }}
+          direction={{ base: "column", md: "row" }}
+        >
+          <Box>
+            <VStack spacing={5}>
+              <FormControl isRequired>
+                <FormLabel fontSize={"sm"}>
+                  What industry do you have experience in?
+                </FormLabel>
 
-                  <Select
-                    size="sm"
-                    isSearchable
-                    options={industries}
-                    chakraStyles={chakraStyles}
-                    selectedOptionStyle="check"
-                  />
-                </FormControl>
+                <Select
+                  size="sm"
+                  isSearchable
+                  options={industries}
+                  chakraStyles={chakraStyles}
+                  selectedOptionStyle="check"
+                />
+              </FormControl>
 
-                <FormControl isRequired>
-                  <FormLabel fontSize={"sm"}>
-                    What&apos;s your role in this industry?
-                  </FormLabel>
+              <FormControl isRequired>
+                <FormLabel fontSize={"sm"}>
+                  What&apos;s your role in this industry?
+                </FormLabel>
 
-                  <Select
-                    size="sm"
-                    isSearchable
-                    options={roles}
-                    selectedOptionStyle="check"
-                    chakraStyles={chakraStyles}
-                  />
-                </FormControl>
-              </VStack>
-              <Box pt={10}>
-                <Button
-                  colorScheme="blue"
-                  bg="blue.400"
-                  color="white"
-                  _hover={{
-                    bg: "blue.500",
-                  }}
-                  isFullWidth
-                  rounded="full"
-                  onClick={() => router.push("/minting-skills/skills-info")}
-                >
-                  Continue
-                </Button>
-              </Box>
+                <Select
+                  size="sm"
+                  isSearchable
+                  options={roles}
+                  selectedOptionStyle="check"
+                  chakraStyles={chakraStyles}
+                />
+              </FormControl>
+            </VStack>
+            <Box pt={10}>
+              <Button
+                colorScheme="blue"
+                bg="blue.400"
+                color="white"
+                _hover={{
+                  bg: "blue.500",
+                }}
+                isFullWidth
+                rounded="full"
+                onClick={() => router.push("/minting-skills/skills-info")}
+              >
+                Continue
+              </Button>
             </Box>
-          </Stack>
-        </VStack>
-      </Box>
-    </MainBox>
+          </Box>
+        </Stack>
+      </VStack>
+    </Box>
   );
 }
